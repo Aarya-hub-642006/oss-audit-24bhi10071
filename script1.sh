d10a905aa1ae0b50b89594a47d5e0fbc82eb3477
Aarya <aaryaunnikrishnan6@gmail.com>
2026-03-29 21:26:01 +0530

    script1 done

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "===== SYSTEM INFO ====="
+
+echo "User: $(whoami)"
+echo "Home Directory: $HOME"
+echo "Kernel Version: $(uname -r)"
+echo "Uptime: $(uptime -p)"
+echo "Date: $(date)"
+
+echo "This system uses open source Linux (GPL License)"
+
+
